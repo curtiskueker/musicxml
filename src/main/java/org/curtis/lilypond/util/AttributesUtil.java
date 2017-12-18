c30445e0a18fa12851e6e239f6d870a35c274b86
curtiskueker <curtiskueker@gmail.com>
2017-12-18 11:00:08 -0800

    reset Attributes values when found

--- src/main/java/org/curtis/lilypond/util/AttributesUtil.java
+++ src/main/java/org/curtis/lilypond/util/AttributesUtil.java
@@ -3,9 +3,15 @@ package org.curtis.lilypond.util;
 import org.curtis.lilypond.PartBuilder;
 import org.curtis.musicxml.attributes.Attributes;
 import org.curtis.musicxml.attributes.Clef;
+import org.curtis.musicxml.attributes.Directive;
+import org.curtis.musicxml.attributes.StaffDetails;
+import org.curtis.musicxml.attributes.Transpose;
 import org.curtis.musicxml.attributes.key.Key;
+import org.curtis.musicxml.attributes.measure.MeasureStyle;
 import org.curtis.musicxml.attributes.time.Time;
+import org.curtis.musicxml.score.PartSymbol;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class AttributesUtil {
@@ -16,9 +22,16 @@ public class AttributesUtil {
     public static void setCurrentAttributes(Attributes attributes) {
         if(attributes == null) return;
 
+        // first time setting
         if(PartBuilder.CURRENT_ATTRIBUTES == null) {
             PartBuilder.CURRENT_ATTRIBUTES = attributes;
             return;
+        }
+
+        // reset only those items found
+        BigDecimal divisions = attributes.getDivisions();
+        if (divisions != null) {
+            PartBuilder.CURRENT_ATTRIBUTES.setDivisions(divisions);
         }
 
         List<Key> keys = attributes.getKeys();
@@ -31,9 +44,45 @@ public class AttributesUtil {
             PartBuilder.CURRENT_ATTRIBUTES.setTimeList(timeList);
         }
 
+        Integer staves = attributes.getStaves();
+        if (staves != null) {
+            PartBuilder.CURRENT_ATTRIBUTES.setStaves(staves);
+        }
+
+        PartSymbol partSymbol = attributes.getPartSymbol();
+        if (partSymbol != null) {
+            PartBuilder.CURRENT_ATTRIBUTES.setPartSymbol(partSymbol);
+        }
+
+        Integer instruments = attributes.getInstruments();
+        if (instruments != null) {
+            PartBuilder.CURRENT_ATTRIBUTES.setInstruments(instruments);
+        }
+
         Clef clef = attributes.getClef();
         if(clef != null) {
             PartBuilder.CURRENT_ATTRIBUTES.setClef(clef);
         }
+
+        List<StaffDetails> staffDetailsList = attributes.getStaffDetailsList();
+        if(!staffDetailsList.isEmpty()) {
+            PartBuilder.CURRENT_ATTRIBUTES.setStaffDetailsList(staffDetailsList);
+        }
+
+        List<Transpose> transpositions = attributes.getTranspositions();
+        if(!transpositions.isEmpty()) {
+            PartBuilder.CURRENT_ATTRIBUTES.setTranspositions(transpositions);
+        }
+
+        List<Directive> directives = attributes.getDirectives();
+        if(!directives.isEmpty()) {
+            PartBuilder.CURRENT_ATTRIBUTES.setDirectives(directives);
+        }
+
+        List<MeasureStyle> measureStyles = attributes.getMeasureStyles();
+        if(!measureStyles.isEmpty()) {
+            PartBuilder.CURRENT_ATTRIBUTES.setMeasureStyles(measureStyles);
+        }
+
     }
 }
