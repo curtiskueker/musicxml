e2e919243651e7132124b4cd026202e1d997e7f6
curtiskueker <curtiskueker@gmail.com>
2018-01-11 10:58:17 -0800

    reinitialize lyric lists after multiple voice build

--- src/main/java/org/curtis/lilypond/LyricPartBuilder.java
+++ src/main/java/org/curtis/lilypond/LyricPartBuilder.java
@@ -1,9 +1,7 @@
 package org.curtis.lilypond;
 
 import org.curtis.lilypond.exception.BuildException;
-import org.curtis.lilypond.exception.TimeSignatureException;
 import org.curtis.lilypond.musicdata.MusicDataBuilder;
-import org.curtis.lilypond.util.TimeSignatureUtil;
 import org.curtis.musicxml.note.Note;
 import org.curtis.musicxml.note.lyric.Extend;
 import org.curtis.musicxml.note.lyric.Lyric;
@@ -35,81 +33,55 @@ public class LyricPartBuilder extends FilteredPartBuilder {
 
         lyricsCounter++;
 
-        // pre-process lyric durations
+        // initialize one lyric list
+        lyricLists = new ArrayList<>();
+        lyricLists.add(new ArrayList<>());
+
         for (Measure measure : part.getMeasures()) {
             for (MusicData musicData : measure.getMusicDataList()) {
                 adjustCurrentDuration(musicData);
 
                 if (musicData instanceof Note) {
-                    Note note = (Note) musicData;
+                    Note note = (Note)musicData;
+                    BigDecimal totalBeats = MathUtil.divide(note.getDuration(), currentDivisions);
                     List<Lyric> lyrics = note.getLyrics();
-                    BigDecimal totalBeats = MathUtil.divide(currentDuration, currentDivisions);
-
-                    if (!lyrics.isEmpty()) {
-                        List<Lyric> additionalLyrics = new ArrayList<>();
-                        for (Lyric lyric : lyrics) {
-                            BigDecimal currentMeasureBeats;
-                            try {
-                                currentMeasureBeats = TimeSignatureUtil.getCurrentMeasureBeats();
-                            } catch (TimeSignatureException e) {
-                                throw new BuildException(e.getMessage());
-                            }
-                            if (MathUtil.largerThan(totalBeats, currentMeasureBeats)) {
-                                while (MathUtil.isPositive(totalBeats)) {
-                                    if (MathUtil.largerThan(totalBeats, currentMeasureBeats)) {
-                                        Lyric additionalLyric = newEmptyLyric();
-                                        additionalLyric.setNumber(lyric.getNumber());
-                                        additionalLyric.setTotalBeats(currentMeasureBeats);
-                                        additionalLyrics.add(additionalLyric);
-                                        totalBeats = MathUtil.subtract(totalBeats, currentMeasureBeats);
-                                    } else {
-                                        lyric.setTotalBeats(totalBeats);
-                                        totalBeats = MathUtil.ZERO;
-                                    }
-                                }
-                            } else {
-                                lyric.setTotalBeats(totalBeats);
+                    if (lyrics.isEmpty()) {
+                        if (lyricLists.size() == 1) {
+                            Lyric emptyLyric = newEmptyLyric();
+                            emptyLyric.setTotalBeats(totalBeats);
+                            MusicDataBuilder musicDataBuilder = new MusicDataBuilder(emptyLyric);
+                            append(musicDataBuilder.build().toString());
+                        } else if (lyricLists.size() > 1) {
+                            for(List<Lyric> lyricList : lyricLists) {
+                                Lyric emptyLyric = newEmptyLyric();
+                                emptyLyric.setTotalBeats(totalBeats);
+                                lyricList.add(emptyLyric);
                             }
                         }
-                        lyrics.addAll(additionalLyrics);
                     } else {
-                        Lyric lyric = newEmptyLyric();
-                        lyric.setTotalBeats(totalBeats);
-                        lyrics.add(lyric);
-                    }
-
-                    currentDuration = MathUtil.ZERO;
-                }
-            }
-        }
-
-        for (Measure measure : part.getMeasures()) {
-            for (MusicData musicData : measure.getMusicDataList()) {
-                if (musicData instanceof Note) {
-                    Note note = (Note)musicData;
-                    List<Lyric> lyrics = note.getLyrics();
-                    if (!lyrics.isEmpty()) {
                         int lyricCount = lyrics.size();
                         if(lyricCount != currentLyricCount) {
                             if (currentLyricCount > 1) {
                                 buildLyricLists();
                             }
-                            if(lyricCount > 1) {
-                                lyricLists = new ArrayList<>(lyricCount);
-                                for(int i = 1; i <= lyricCount; i++) {
-                                    lyricLists.add(new ArrayList<>());
-                                }
+
+                            lyricLists = new ArrayList<>(lyricCount);
+                            for(int i = 1; i <= lyricCount; i++) {
+                                lyricLists.add(new ArrayList<>());
                             }
                         }
 
                         if (lyricCount == 1) {
-                            MusicDataBuilder musicDataBuilder = new MusicDataBuilder(lyrics.get(0));
+                            Lyric lyric = lyrics.get(0);
+                            lyric.setTotalBeats(totalBeats);
+                            MusicDataBuilder musicDataBuilder = new MusicDataBuilder(lyric);
                             append(musicDataBuilder.build().toString());
                         } else if (lyricCount > 1) {
                             for(Lyric lyric : lyrics) {
                                 Integer lyricNumber = StringUtil.getInteger(lyric.getNumber());
                                 if (lyricNumber ==  null) throw new BuildException("Invalid lyric number: " + lyric.getNumber());
                                 if (lyricNumber > lyricCount) throw new BuildException("Expecting consecutive numbered lyric numbers: " + lyric.getNumber());
+                                lyric.setTotalBeats(totalBeats);
                                 lyricLists.get(lyricNumber - 1).add(lyric);
                             }
                         }
