aef6a6880783f5d25158985e04d9016a0bec0209
curtiskueker <curtiskueker@gmail.com>
2018-01-29 16:33:50 -0800

    wavy line empty method warning

--- src/main/java/org/curtis/lilypond/musicdata/OrnamentBuilder.java
+++ src/main/java/org/curtis/lilypond/musicdata/OrnamentBuilder.java
@@ -46,6 +46,7 @@ public class OrnamentBuilder extends MusicDataBuilder {
 
     // TODO: wavy line implementation
     public StringBuilder buildWavyLine(WavyLine wavyLine) {
+        System.err.println("Warning OrnamentBuilder.buildWavyLine not implemented");
         return stringBuilder;
     }
 
