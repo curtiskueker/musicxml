c341f57d2ce07956247d15e6991f5090f11f748f
curtiskueker <curtiskueker@gmail.com>
2018-05-15 10:27:22 -0700

    annotate dates

--- src/main/java/org/curtis/musicxml/identity/encoding/EncodingDate.java
+++ src/main/java/org/curtis/musicxml/identity/encoding/EncodingDate.java
@@ -1,14 +1,14 @@
 package org.curtis.musicxml.identity.encoding;
 
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.Transient;
 import java.util.Date;
 
 @Entity
 @DiscriminatorValue("encoding date")
 public class EncodingDate extends Encoding {
-    @Transient
+    @Column(name = "encoding_date")
     private Date encodingDate;
 
     public EncodingDate() {
--- src/main/java/org/curtis/util/DateUtil.java
+++ src/main/java/org/curtis/util/DateUtil.java
@@ -26,4 +26,23 @@ public class DateUtil {
     public static Date parseDate(String dateString) {
         return parseDate(dateString, YYYY_MM_DD);
     }
+
+    private static String formatDate(Date dateIn, DateFormat dateFormat) {
+        try {
+            String output = dateFormat.format(dateIn);
+            if (StringUtil.isEmpty(output)) output = "";
+
+            return output;
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    public static String formatDate(Date dateIn, String formatString) {
+        return formatDate(dateIn, new SimpleDateFormat(formatString));
+    }
+
+    public static String formatDate(Date dateIn) {
+        return formatDate(dateIn, YYYY_MM_DD);
+    }
 }
