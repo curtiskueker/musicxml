f6207dbfa21e9e6bf0ecf44ff41df471932cddee
curtiskueker <curtiskueker@gmail.com>
2019-01-21 15:08:54 -0800

    convert remaining 2Pdf executables to conversion method

--- src/main/java/org/curtis/musicxml/bin/Db2Pdf.java
+++ src/main/java/org/curtis/musicxml/bin/Db2Pdf.java
@@ -1,15 +1,7 @@
 package org.curtis.musicxml.bin;
 
-import org.curtis.lilypond.ScoreBuilder;
+import org.curtis.lilypond.exception.BuildException;
 import org.curtis.musicxml.exception.MusicXmlException;
-import org.curtis.musicxml.score.Score;
-import org.curtis.properties.AppProperties;
-
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Db2Pdf extends MusicXmlScript {
     public Db2Pdf() {
@@ -19,25 +11,8 @@ public class Db2Pdf extends MusicXmlScript {
     public void execute() throws MusicXmlException {
         try {
             SKIP_COMMENTS = true;
-            Score score = getScoreFromDb();
-            ScoreBuilder scoreBuilder = new ScoreBuilder(score);
-
-            List<String> commands = new ArrayList<>();
-            commands.add(AppProperties.getString("location.lilypond"));
-            commands.add("-fpdf");
-            commands.add("-o" + OUTPUT_FILE);
-            commands.add("-");
-            Process process = new ProcessBuilder(commands).start();
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
-            writer.write(scoreBuilder.build().toString());
-            writer.flush();
-            writer.close();
-            InputStream errorStream = process.getErrorStream();
-            int c;
-            while ((c = errorStream.read()) != -1) {
-                System.err.print((char)c);
-            }
-        } catch (Exception e) {
+            convertLilypondToPdf(getLilypondFromScore(getScoreFromDb()));
+        } catch (BuildException e) {
             throw new MusicXmlException(e);
         }
     }
--- src/main/java/org/curtis/musicxml/bin/MusicXml2Pdf.java
+++ src/main/java/org/curtis/musicxml/bin/MusicXml2Pdf.java
@@ -1,17 +1,8 @@
 package org.curtis.musicxml.bin;
 
-import org.curtis.lilypond.ScoreBuilder;
 import org.curtis.musicxml.exception.MusicXmlException;
-import org.curtis.musicxml.handler.ScoreHandler;
-import org.curtis.musicxml.score.Score;
-import org.curtis.properties.AppProperties;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MusicXml2Pdf extends MusicXmlScript {
     public MusicXml2Pdf() {
@@ -20,27 +11,8 @@ public class MusicXml2Pdf extends MusicXmlScript {
 
     public void execute() throws MusicXmlException {
         try {
-            File xmlFile = new File(INPUT_FILE);
             SKIP_COMMENTS = true;
-            ScoreHandler scoreHandler = handleXmlScoreFile(xmlFile);
-            Score score = scoreHandler.getScore();
-            ScoreBuilder scoreBuilder = new ScoreBuilder(score);
-
-            List<String> commands = new ArrayList<>();
-            commands.add(AppProperties.getString("location.lilypond"));
-            commands.add("-fpdf");
-            commands.add("-o" + OUTPUT_FILE);
-            commands.add("-");
-            Process process = new ProcessBuilder(commands).start();
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
-            writer.write(scoreBuilder.build().toString());
-            writer.flush();
-            writer.close();
-            InputStream errorStream = process.getErrorStream();
-            int c;
-            while ((c = errorStream.read()) != -1) {
-                System.err.print((char)c);
-            }
+            convertLilypondToPdf(getLilypondFromScore(handleXmlScoreFile(new File(INPUT_FILE)).getScore()));
         } catch (Exception e) {
             throw new MusicXmlException(e);
         }
