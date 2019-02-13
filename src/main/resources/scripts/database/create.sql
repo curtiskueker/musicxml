94340fd14b036ef623f4aeeb8c653fed5d75d38a
curtiskueker <curtiskueker@gmail.com>
2019-02-12 16:56:53 -0800

    remove create.sql

--- src/main/resources/scripts/database/create.sql
+++
@@ -1,8 +0,0 @@
--- mysql statements for create database and user, grants
--- edit as necessary for other dialects, usernames, etc.
-create user 'musicxml' identified by 'musicxml';
-grant all on musicxml.* to 'musicxml';
-
-drop database musicxml;
-create database musicxml;
-use musicxml;
