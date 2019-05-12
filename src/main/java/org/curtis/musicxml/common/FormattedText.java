c280ad26dd99da75cfe6f414f9e28db6c6874f12
curtiskueker <curtiskueker@gmail.com>
2019-05-12 14:45:03 -0700

    remove import statement

--- src/main/java/org/curtis/musicxml/common/FormattedText.java
+++ src/main/java/org/curtis/musicxml/common/FormattedText.java
@@ -4,7 +4,6 @@ import org.apache.commons.text.StringEscapeUtils;
 import org.curtis.database.DatabaseItem;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
